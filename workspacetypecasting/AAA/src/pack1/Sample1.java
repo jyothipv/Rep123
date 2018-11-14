9a0fe222aa7e5f74b0d8781d8d362a3f409462f8
unknown <venkatajyothi.pendyala@gmail.com>
2018-11-14 17:45:40 +0800

    typecasting

---
+++ workspacetypecasting/AAA/src/pack1/Sample1.java
@@ -0,0 +1,15 @@
+package pack1;
+
+public class Sample1 {
+ public void m1(){
+	 System.out.println("m1");
+ }
+  void m2(){
+	 System.out.println("m2");
+ }
+ public void m3(){
+	 System.out.println("m3");
+ }
+
+
+}
---
+++ workspacetypecasting/AAA/src/pack1/Sample2.java
@@ -0,0 +1,21 @@
+package pack1;
+
+public class Sample2 extends Sample1{
+	public void m4(){
+		 System.out.println("m4");
+	 }
+	public void m5(){
+		 System.out.println("m5");
+	 }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Sample1 s=new Sample2();
+Object s1=(Sample2)s;
+
+
+
+
+
+	}
+
+}
